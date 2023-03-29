c34847b2228a316ffb8d2c356da7f446d5ec0d44
hmzhan <hmzhan@gmail.com>
2023-03-29 08:00:08 -0400

    add merge and get functions

--- src/config.py
+++ src/config.py
@@ -6,8 +6,11 @@ class Config:
         self.config_file = config_file
         self.config = yaml.load(open(self.config_file, "r"), Loader=yaml.FullLoader)
 
-    def _merge(self):
-        return
+    def _merge(self, default_config, active_config):
+        return {**default_config, **active_config}
 
     def get(self, proj_env):
-        return
+        default_config = self.config["default"]
+        active_config = self.config[proj_env]
+        merged_config = self._merge(default_config, active_config)
+        return merged_config
