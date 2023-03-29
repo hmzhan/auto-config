65ecd3d7f25ebc806a8abfa9f6e8555e5968afc0
hmzhan <hmzhan@gmail.com>
2023-03-29 08:04:43 -0400

    add docstring to merge and get functions

--- src/config.py
+++ src/config.py
@@ -7,9 +7,25 @@ class Config:
         self.config = yaml.load(open(self.config_file, "r"), Loader=yaml.FullLoader)
 
     def _merge(self, default_config, active_config):
-        return {**default_config, **active_config}
+        """
+        Merge two dictionaries
+        :param default_config: default configurations
+        :param active_config: active configurations
+        :return: a merged dictionary
+        """
+        if len(default_config) == 0:
+            return default_config
+        elif len(active_config) == 0:
+            return active_config
+        else:
+            return {**default_config, **active_config}
 
     def get(self, proj_env):
+        """
+        Get configurations for an active proj environment
+        :param proj_env: active project environment: qa, prod, etc.
+        :return: a dictionary that contains default and active config
+        """
         default_config = self.config["default"]
         active_config = self.config[proj_env]
         merged_config = self._merge(default_config, active_config)
