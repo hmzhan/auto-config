af788c2fb275269f4c88f1868304a9566f9009a0
hmzhan <hmzhan@gmail.com>
2023-04-01 08:12:58 -0400

    add raise exception and change to YamlLoader

--- src/auto_config.py
+++ src/auto_config.py
@@ -1,10 +1,18 @@
-import yaml
+
+from piny import YamlLoader
+from pathlib import Path
 
 
 class Config:
     def __init__(self, config_file):
         self.config_file = config_file
-        self.config = yaml.load(open(self.config_file, "r"), Loader=yaml.FullLoader)
+        self.config = self._load_config()
+
+    def _load_config(self):
+        if Path(self.config_file).is_file():
+            return YamlLoader(path=self.config_file).load()
+        else:
+            raise Exception(f"{self.config_file} not found.")
 
     @staticmethod
     def _merge(default_config, active_config):
@@ -27,10 +35,13 @@ class Config:
         :param proj_env: active project environment: qa, prod, etc.
         :return: a dictionary that contains default and active config
         """
-        default_config = self.config["default"]
-        try:
+        if "default" in self.config:
+            default_config = self.config["default"]
+        else:
+            raise Exception("You must provide a default configuration")
+        if proj_env in self.config:
             active_config = self.config[proj_env]
-            merged_config = self._merge(default_config, active_config)
-            return merged_config
-        except:
-            print(f"Please provide {proj_env} parameters in {self.config_file}")
+        else:
+            raise Exception(f"Please provide {proj_env} parameters in {self.config_file}")
+        merged_config = self._merge(default_config, active_config)
+        return merged_config
